f8bc5f6859d1db3b79bd164e1809e4d739a280f0
Mohamed Ashraf <75941628+Mohamed-Ashraf1@users.noreply.github.com>
2024-09-27 18:55:00 +0300

    Time: 7 ms (65.04%), Space: 47.5 MB (81.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        String[] sortedArr=new String[strs.length];
-        List<List<String>> result=new ArrayList();
+   
+         Map<String, List<String>> count = new HashMap<>();
         for(int i=0;i<strs.length;i++){
             char[] temp=strs[i].toCharArray();
             Arrays.sort(temp);
-            sortedArr[i]=new String(temp);
-        }
-        Map<String, List<Integer>> count = new HashMap<>();
-        for(int i=0;i<strs.length;i++){
-            List<Integer> indices = count.getOrDefault(sortedArr[i], new ArrayList<>());
-            indices.add(i);
-            count.put(sortedArr[i], indices);
-           
-        }
-        for (List<Integer> list : count.values()) {
-            List<String> l=new ArrayList();
-            for(int i:list){
-                l.add(strs[i]);
+            String word=new String(temp);
+            if(!count.containsKey(word)){
+                count.put(word, new ArrayList<>());
             }
-            result.add(l);
-
+            count.get(word).add(strs[i]);
         }
-        return result;
+       
+    
+       
+        return new ArrayList<>(count.values());
     }
 }
